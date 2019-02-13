d69a27db3c72bb811d20ceaa4c6e94129c55b533
Blake Scott <blakerscott@gmail.com>
2019-02-12 22:22:24 -0800

    add youtube.js

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyAekvqr4w_kmF3KsuWExvmh31vpytIb8sg';
+
+export default axios.create({
+    baseURL: 'https:www.googleapis.com/youtube/v3',
+    params: {
+      part: 'snippet',
+      maxResults: 5,
+      key: KEY
+    }
+})
