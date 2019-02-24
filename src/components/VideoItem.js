d552aafe62ada2e680a2bfedc582d190873693cb
Blake Scott <blakerscott@gmail.com>
2019-02-24 15:03:26 -0800

    add description and thumbnail to each instance of VideoItem

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const VideoItem = (props) => {
-  return <div>Hi</div>;
+//Note: ({ videos }) == (props.videos), which allows you to write things
+//like videos.list instead of props.videos.list
+const VideoItem = ({ video }) => {
+  return ( 
+    <div>
+      <img src={video.snippet.thumbnails.medium.url} />
+      {video.snippet.title}
+    </div>
+  );
 };
 
 export default VideoItem;
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideoList = (props) => {
-  return <div>{props.videos.length}</div>;
+//Note: ({ videos }) == (props.videos), which allows you to write things
+//like videos.list instead of props.videos.list
+const VideoList = ({ videos }) => {
+  const renderedList = videos.map(video => {
+    return <VideoItem video={video} />;
+});
+
+    return <div>{renderedList}</div>;
 };
 
 export default VideoList;
