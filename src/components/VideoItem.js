df8314b73eb2b0e72a68a4659e62b412f930ac0b
Blake Scott <blakerscott@gmail.com>
2019-02-24 15:37:52 -0800

    add some custom styling to the VideoItem

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,11 @@
+import './VideoItem.css'
 import React from 'react';
 
 //Note: ({ videos }) == (props.videos), which allows you to write things
 //like videos.list instead of props.videos.list
 const VideoItem = ({ video }) => {
   return ( 
-    <div className="item">
+    <div className="video-item item">
       <img className="ui image" src={video.snippet.thumbnails.medium.url} />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
