a44cda427ea2da6e719e1cf9a77c9ab48cac8ab4
Blake Scott <blakerscott@gmail.com>
2019-04-07 15:35:42 -0700

    add a pre selected video after a search is made

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,10 @@ class App extends React.Component {
       }
     });
 
-    this.setState({videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[1]
+    });
 };
 
 onVideoSelect = video => {
@@ -25,10 +28,19 @@ onVideoSelect = video => {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList 
-          onVideoSelect={this.onVideoSelect} 
-          videos={this.state.videos}/>
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList 
+                onVideoSelect={this.onVideoSelect} 
+                videos={this.state.videos}
+              />
+            </div>
+          </div>  
+        </div>
       </div>
     );
   }
