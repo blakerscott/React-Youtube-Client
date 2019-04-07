31cbf3feabbf3d663eb751e2ca74a41b050fe9fb
Blake Scott <blakerscott@gmail.com>
2019-04-07 14:57:42 -0700

    add video player to the video detail component

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,9 +5,13 @@ const VideoDetail = ({ video }) => {
     return <div>Loading...</div>;  
   }
 
+  const VideoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 
   return ( 
-    <div>  
+    <div>
+      <div className="ui embed">
+       <iframe src={VideoSrc} />
+      </div>  
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
         <p>{video.snippet.description}</p>
